00f07a74689d43382432680a66e24b4d9be9adf4
dongxiaohuang <dongxiao.huang17@imperial.ac.uk>
2018-04-02 16:47:38 +0100

    modify the rectangle module into asyn, add callback and error handling Node Callbacks and Error Handling

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-var rect = {
-  perimeter : (x,y) => (2*(x+y)),
-  area : (x,y) => (x*y)
-}
+// var rect = {
+//   perimeter : (x,y) => (2*(x+y)),
+//   area : (x,y) => (x*y)
+// }
+var rect = require('./rectangle');
 
-function solveRect(l, w){
-  console.log("solving the rectangle with length = "+ l+ " and width = " + w);
-  if(l <= 0 || w <= 0){
-    console.log("width = " +w +" and length = "+l + ' must be greater than zero');
-  }else{
-    console.log("perimeter = " + rect.perimeter(l,w));
-    console.log("area = "+ rect.area(l,w));
-  }
+function solveRect(l, w) {
+  console.log("solving the rectangle with length = " + l + " and width = " + w);
+  rect(l, w, (error, rectangle) => {
+    if(error) {
+      console.log("ERROR:" + error.message);
+    }
+    else {
+      console.log("perimeter = " + rectangle.perimeter());
+      console.log("area = " + rectangle.area());
+    }
+  });
+  console.log("this is a statement after callback");
 }
-solveRect(1,2);
-solveRect(3,4);
-solveRect(0,4);
-solveRect(-1,-1);
+solveRect(1, 2);
+solveRect(3, 4);
+solveRect(0, 4);
+solveRect(-1, -1);
--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,22 @@
 exports.perimeter = (x,y) => (2*(x+y));
 exports.area = (x,y) => (x*y);
+module.exports = (x, y, callback) => {
+  if(x <= 0 || y <= 0){
+    //callback(error_return, normal_return);
+    // setTimeout(callback, timep)
+    setTimeout(() => callback(
+      new Error("width = " +x +" and length = "+y + ' must be greater than zero') // return value
+      ,null //return value
+    ), 2000);
+    // set timeout to imitate the time from fetch data from database, this callback function will not run until the 2000 timeout ends;
+  }
+else{
+  setTimeout(() =>
+    callback(null // return value,
+      {
+        perimeter: () => (2*(x+y)), // closure of javascript, we dont need to add (x,y)
+        area: () => (x*y)
+      }// return value
+    )
+    ,2000);}
+}
