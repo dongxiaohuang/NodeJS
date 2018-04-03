299ba9733cf32e6470ca807c721a15d04eecdf53
dongxiaohuang <dongxiao.huang17@imperial.ac.uk>
2018-04-03 21:43:33 +0100

    restore index.js

---
+++ node-express/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const http = require('http');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const dishRouter = require('./routes/dishRouter');
+const pRouter = require('./routes/promotionsRouter');
+const leaderRouter = require('./routes/leadersRouter');
+
+let hostname = 'localhost';
+let port = 3000;
+let app = express();
+app.use(morgan('dev')); // middleware / dev version
+app.use(express.static(__dirname + '/public')); // __dirname is the root of the project, don't forget '/'
+app.use(bodyParser.json()) // use this middleware to parse the json data inside requst body
+
+// app.use('/dishes', dishRouter);
+app.use('/dishes', dishRouter);
+app.use('/promotions', pRouter);
+app.use('/leaders', leaderRouter);
+
+app.use((req, res, next) => {
+  console.log(req.headers);
+  res.statusCode = 200;
+  res.setHeader("Content-Type", 'text/html');
+  res.end('<html><head><body><h1>This is an express server</h1></body></head></html>');
+});
+
+let server = http.createServer(app);
+
+server.listen(port, hostname, () =>
+{
+  console.log(`Server running at http://${hostname}:${port}`);
+});
