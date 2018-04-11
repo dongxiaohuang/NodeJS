3cb8033400c21abbe3cf841beaefbae5a9090a33
dongxiaohuang <dongxiao.huang17@imperial.ac.uk>
2018-04-11 20:00:12 +0100

    Set up a method for users to register. Authenticate registered users to enable them to access secure resources. Enable clients to access secure resources on the server after authentication.

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -14,7 +14,7 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var leaderRouter = require('./routes/leadersRouter');
 var promotionsRouter = require('./routes/promotionsRouter');
-
+var usersRouter = require('./routes/users');
 var app = express();
 
 var url = 'mongodb://localhost:27017/conFusion'
@@ -34,61 +34,37 @@ app.use(cookieParser('12345bdksjhbksb')); // string to be used for encoding
 
 app.use(session({
     name: 'session-id',
-    secret: '12345-67890-09876-54321',
-    saveUninitializied: false,
+    secret: '12345-67890-09876-54321', //a secret key
+    saveUninitialized: false,
     resave: false,
     store: new FileStore()
 }));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 function auth(req, res, next) {
     console.log(req.session);
 
     if(!req.session.user){
-        console.log(req.headers);
-        var authHeader = req.headers.authorization;
-        if(!authHeader){
-            var err = new Error('You are not authorized!');
-            res.setHeader('WWW-Authenticate', 'Basic');// pop up log-in window
-            err.status = 401;
-            return next(err);
-        }
-
-        var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString()
-        .split(':');
-        var user = auth[0];
-        var psw = auth[1];
-        // The identity (===) operator behaves identically to the equality (==)
-        // operator except no type conversion is done,
-        //and the types must be the same to be considered equal.
-
-        if(user === 'admin' && psw === 'password'){
-            req.session.user = 'admin';
-            next(); //authorized
-        }else{
-            var err = new Error('You are not authorized!');
-            res.setHeader('WWW-Authenticate', 'Basic');// pop up log-in window
-            err.status = 401;
-            return next(err);
-        }
+        var err = new Error('You are not authorized!');
+        err.status = 403;
+        return next(err);
     }else{
-        if(req.session.user === 'admin'){
-            console.log('req.session: ', req.session);
+        if(req.session.user === 'authenticated'){
             next();
         }else{
             var err = new Error('You are not authorized!');
-            err.status = 401;
+            err.status = 403;
             return next(err);
         }
     }
-
 }
 
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', dishRouter);
+
 app.use('/promotions', promotionsRouter);
 app.use('/dishes', dishRouter);
 app.use('/leaders', leaderRouter);
